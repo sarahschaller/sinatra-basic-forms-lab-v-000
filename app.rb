35504f0ddbd75a8f02e8b2edb2e7c69693db0bf6
Sarah Schaller <sarcsch@aol.com>
2017-10-07 17:35:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,29 +1,6 @@
-# require_relative 'config/environment'
-#
-# class App < Sinatra::Base
-#
-#   get '/' do
-#     erb :index
-#   end
-#
-#   get '/new' do
-#     erb :create_puppy
-#   end
-#
-#   post '/' do
-#     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
-#
-#     erb = :display_puppy
-#   end
-#
-#
-#
-# end
-
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
 
   get '/' do
     erb :index
@@ -33,9 +10,12 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/puppy' do
+  post '/' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
-    erb :display_puppy
+
+    erb = :display_puppy
   end
+
+
 
 end
