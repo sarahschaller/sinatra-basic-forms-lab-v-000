2f98028eb41b46dcf34d0329ddb1d2b9c8e808b5
Sarah Schaller <sarcsch@aol.com>
2017-10-07 17:31:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/puppy' do
+  post '/' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
 
     erb = :display_puppy
